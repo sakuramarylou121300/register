35cd14e1961be097bbd22eca6c5b5c326b9bfff6
Mary Lou D. Cortez <maryloucortez12@gmail.com>
2023-12-28 09:18:56 +0800

    upload-photo

--- app/Livewire/RegisterForm.php
+++ app/Livewire/RegisterForm.php
@@ -3,9 +3,47 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use App\Models\Register;
+use Livewire\WithPagination;
+use Livewire\WithFileUploads;
+
 
 class RegisterForm extends Component
 {
+    use WithFileUploads;
+
+    #[Rule('required| min:3| max:50')]
+    public $name;
+
+    #[Rule('required| email| max:255| unique:registers')]
+    public $email;
+
+    #[Rule('required| min:3')]
+    public $password;
+
+    #[Rule('nullable|sometimes|image|max:1024')]
+    public $image;
+
+    public function create(){
+        // VALIDATE 
+        $validate = $this->validate();
+        Register::create($validate);
+
+        // THIS IS FOR THE PHOTO
+        if($this->image){
+            $validate['image']=$this->image->store('uploads', 'public');
+        }
+
+        // REMOVE VALUES AFTER CREATE
+        $this->reset('name', 'email', 'password', 'image');
+
+        // GIVE MESSAGE AFTER CREATE
+        session()->flash('success', 'Created');
+
+        // // RESET PAGE AFTER, GO TO FIRST PAGE
+        // $this->resetPage();
+    }
     public function render()
     {
         return view('livewire.register-form');
--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -1,3 +1,56 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="mt-10 p-5 mx-auto sm:max-w-sm shadow border-teal-500 border-t-2">
+
+    <div class="flex">
+        <h2 class="text-center font-semibold text-2x text-gray-800 mb-5">Create New Account</h2>
+    </div>
+
+    <!-- IF SUCCESS CREATE THEN FLASH A MESSAGE FROM BACKEND -->
+    @if (session('success'))
+        <span class="text-green-500">{{session('success')}}</span>
+    @endif
+
+    <!-- CREATE FROM BACKEND CREATE FUNCTION -->
+    <form wire:submit="create">
+        <label class="mt-3 block text-sm font-medium leading-6  text-gray-900">Name</label>
+        <input wire:model='name' type="text" id="name" placeholder="name.."
+        class="ring-1 ring-inset ring-gray-300 bg-gray-100 text-gray-900 text-sm rounded block w-full px-4  py-2">
+        @error('name')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <label class="mt-3 block text-sm font-medium leading-6  text-gray-900">Email</label>
+        <input wire:model='email' type="email" id="name" placeholder="email.."
+        class="ring-1 ring-inset ring-gray-300 bg-gray-100 text-gray-900 text-sm rounded block w-full px-4  py-2">
+        @error('email')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <label class="mt-3 block text-sm font-medium leading-6  text-gray-900">Password</label>
+        <input wire:model='password' type="password" id="password" placeholder="password.."
+        class="ring-1 ring-inset ring-gray-300 bg-gray-100 text-gray-900 text-sm rounded block w-full px-4  py-2">
+        @error('password')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <label class="mt-3 block text-sm font-medium leading-6  text-gray-900">Profile Picture</label>
+        <input wire:model="image" type="file" accept="image/png, image/jpeg" id="image" 
+        class="ring-1 ring-inset ring-gray-300 bg-gray-100 text-gray-900 text-sm rounded block w-full px-4  py-2">
+        @error('image')
+        <span class="text-red-500 text-xs">{{$message}}</span>
+        @enderror
+
+        <!-- TO DISPLAY IMAGE -->
+        @if($image)
+            <img src="{{$image->temporaryUrl()}}" alt=""
+            class="rounded w-20 h-20 mt-5 block">
+        @endif
+
+        <!-- LOAD THIS WHEN WIRE IS LOADING OR WHEN THE UPLOAD IS LOADING -->
+        <div wire:loading wire:target="image">
+            <span class="text-green-500">Uploading ...</span>
+        </div>
+
+        <button type="submit"
+        class="block mt-3 px-4  py-2 bg-teal-500 text-white font-semibold rounded hover:bg-teal-600">Create</button>
+    </form>
 </div>
